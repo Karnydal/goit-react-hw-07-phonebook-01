92583275fff2db098080bfacd25f466a81fa0707
Karnydal <97188586+Karnydal@users.noreply.github.com>
2022-10-01 00:27:50 +0300

    new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
+import ContactForm from 'components/ContactForm/ContactForm';
+import ContactList from 'components/ContactList/ContactList';
+import ContactFilter from 'components/ContactFilter/ContactFilter';
+import Section from 'components/Section/Section';
+import { useGetContactsQuery } from 'redux/contactsApi';
+
+function App() {
+  const { data: contacts, isLoading } = useGetContactsQuery();
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Section>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+
+      {contacts?.length > 0 && (
+        <>
+          <ContactFilter />
+        </>
+      )}
+      {isLoading && <p>Loading...</p>}
+      {contacts?.length === 0 && !isLoading && <p>Contact list is empty</p>}
+      <ContactList />
+    </Section>
   );
-};
+}
+
+export default App;
